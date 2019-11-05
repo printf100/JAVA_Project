bb56f0b254dffaf799bf5942a539b3b398d5ffde
printf100 <mickey0502@naver.com>
2019-11-05 21:14:53 +0900

    랜덤 로또 번호 생성기

---
+++ Lotto/src/com/lotto/Lotto.java
@@ -0,0 +1,32 @@
+package com.lotto;
+
+import java.util.Arrays;
+
+// 로또 번호 생성기
+
+public class Lotto {
+	
+	public static void main(String[] args) {
+		
+		int Lotto[] = new int[6];
+		
+		for(int i=0; i<Lotto.length; i++) {
+			Lotto[i] = (int) (Math.random() * 45 + 1);	// 로또 번호는 1~45번
+			
+			// 중복 제거
+			for(int j=0; j<i; j++) {
+				if(Lotto[j] == Lotto[i]) {
+					i--;
+					break;
+				}
+			}
+		}
+		
+		// 배열 정렬
+		Arrays.sort(Lotto);
+		
+		for(int i=0; i<Lotto.length; i++) {
+			System.out.print(Lotto[i] + " ");
+		}
+	}
+}
